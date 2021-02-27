1bf643a96c31f5d0e4dce9d3148c4f35d8d9b7ac
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-02-27 22:17:08 +0700

    Handle exceptions

--- problems-3/o-markelov/problem4.py
+++ problems-3/o-markelov/problem4.py
@@ -2,10 +2,16 @@
 
 class CartesianProduct:
     def __init__(self, objects, n):
-        self._cursor = 0
-        self._products = [[]]
-        for _ in range(n):
-            self._products = [product + [obj] for product in self._products for obj in objects]
+        try:
+            if n < 1:
+                raise ValueError('"n" must be positive') from None
+
+            self._cursor = 0
+            self._products = [[]]
+            for _ in range(n):
+                self._products = [product + [obj] for product in self._products for obj in objects]
+        except TypeError:
+            raise ValueError('"objects" must be of "Iterable" type; "n" must be of "int" type') from None
 
     def current(self):
         return tuple(self._products[self._cursor]) if self._cursor < len(self._products) else None
